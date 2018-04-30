12875abe7966107127b3b56d32e39a4dfdcd4943
Yonatan Tal <ytarlovs@gmail.com>
2018-04-30 17:56:47 +0300

    Bpms conversion

---
+++ HeartRate/BmpsToSegmentsConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HeartRate
+{
+    public class BmpsToSegmentsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            IList<int> bpms = (IList<int>)value;
+            IList<Segment> segments = new List<Segment>();
+            IList<Point> normalizedToGraphBpms = new List<Point>();
+            for(int i = 0; i < bpms.Count; i++)
+            {
+                Point point = new Point();
+                if(i == 0)
+                {
+                     point = new Point(0, bpms[i] * (300 / 150));  
+                }
+                else
+                {
+                    point = new Point(normalizedToGraphBpms[i-1].X + 500 / bpms.Count, bpms[i] * (300 / 150));
+                }
+                normalizedToGraphBpms.Add(point);
+            }
+            segments = new List<Segment>(normalizedToGraphBpms.Zip(normalizedToGraphBpms.Skip(1), (a, b) => new Segment { From = a, To = b }));
+            return segments;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HeartRate/HeartRateViewModel.cs
+++ HeartRate/HeartRateViewModel.cs
@@ -2,7 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 
-namespace HeartRate {
+namespace HeartRate
+{
     public class HeartRateViewModel : Caliburn.Micro.PropertyChangedBase, IHeartRate
     {
         public HeartRateViewModel(IHeartRateListener p_heartRateListener)
@@ -14,9 +15,20 @@ namespace HeartRate {
                 new Point { X = 20, Y = 20 },
             };
 
+            
             Segments = new List<Segment>(Points.Zip(Points.Skip(1), (a, b) => new Segment { From = a, To = b }));
+            Bpms = new List<int>();
+            Bpms.Add(200);
+            Bpms.Add(58);
+            Bpms.Add(67);
+            Bpms.Add(83);
+            Bpms.Add(94);
+            Bpms.Add(76);
+            Bpms.Add(112);
+            Bpms.Add(50);
         }
 
+        public IList<int> Bpms { get; set; }
         public IList<Segment> Segments { get; set; }
     }
 
